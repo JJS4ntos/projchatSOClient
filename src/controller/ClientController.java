39b56dd249828333b268626ed4348340a711c183
Junior Santos <jsantos.class@gmail.com>
2017-11-14 13:38:40 -0300

    Sistema de envio de emoticon

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -4,12 +4,12 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import io.Client;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -18,6 +18,7 @@ import view.Home;
 
 public class ClientController implements Initializable {
 
+    @FXML private Label lbl_sorriso, lbl_sorriso2, lbl_gargalhada, lbl_pensativo, lbl_surpreso, lbl_surpreso2, lbl_raiva, lbl_olholado;
 	@FXML private TextField tf_mensagem;
 	@FXML private ListView<String> list_mensagens;
 	@FXML private ImageView img_sender;
@@ -37,7 +38,18 @@ public class ClientController implements Initializable {
 				tf_mensagem.clear();
 			}
 		});
-		Platform.runLater(()->Home.stage.setOnCloseRequest(e->client.sendMessage("REMOVER$chat")));
+		lbl_sorriso.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_sorriso.getText()));
+		lbl_sorriso2.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_sorriso2.getText()));
+		lbl_gargalhada.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_gargalhada.getText()));
+		lbl_pensativo.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_pensativo.getText()));
+		lbl_surpreso.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_surpreso.getText()));
+		lbl_surpreso2.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_surpreso2.getText()));
+		lbl_raiva.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_raiva.getText()));
+		lbl_olholado.setOnMouseClicked(e->tf_mensagem.setText(tf_mensagem.getText()+lbl_olholado.getText()));
+		Platform.runLater(()->Home.stage.setOnCloseRequest(e->{
+			client.sendMessage("Hey pessoal, estou me desconectando.");
+			client.sendMessage("REMOVER$chat");
+		}));
 	}
 	
 	private void criarConexao() {
@@ -55,7 +67,7 @@ public class ClientController implements Initializable {
 		Task<Void> task= new Task<Void>() {
 			protected Void call() throws IOException {
 				while(true) {
-					String[] mensagem=in.readUTF().split("¨¨");
+					String[] mensagem=in.readUTF().split("Â¨Â¨");
 					Platform.runLater(()->list_mensagens.getItems().add(mensagem[0].concat(": ").concat(mensagem[1])));
 				}
 			}
--- src/io/Client.java
+++ src/io/Client.java
@@ -2,21 +2,18 @@ package io;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 
-import javafx.scene.image.Image;
-
 public class Client {
 	
 	private final Socket socket;
-	private ObjectOutputStream out;
+	private DataOutputStream out;
 	
 	public Client(String ip, int porta) throws UnknownHostException, IOException {
 		socket= new Socket(ip, porta);
-		out= new ObjectOutputStream(socket.getOutputStream());
+		out= new DataOutputStream(socket.getOutputStream());
 	}
 
 	public Socket getSocket() {
@@ -28,17 +25,6 @@ public class Client {
 			out.writeUTF("[".concat(LocalDateTime.now().toString()).concat("]").concat(socket.getInetAddress().getHostAddress().concat("ии").concat(mensagem)));
 			out.flush();
 		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void sendImage(Image image) {
-		try {
-			ObjectOutputStream out= new ObjectOutputStream(socket.getOutputStream());
-			out.writeObject(image);
-			out.flush();
-			out.close();
-		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
